dca275f76e6338481ca92d3c5f34c16194d00ade
amr-Magdy-PT <95287975+amr-Magdy-PT@users.noreply.github.com>
2022-06-06 15:11:13 +0200

    Adding SPM Support (#81)
    Support SPM Package

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "PayTabsSDK",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "PayTabsSDK",
+            targets: ["PaymentSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        .binaryTarget(name: "PaymentSDK", url: "https://github.com/paytabscom/paytabs-ios-library-sample/raw/master/sources/PaymentSDK.xcframework.zip", checksum: "2d904fb85aa0b1873061f4eb1323ce60d412a28dd04b55579820ba908b8d3aa3")
+    ]
+)
