4a9799633526d425b35f962cca1542c781ae9cac
Amr <amr.magdy@paytabs.com>
2022-06-06 14:51:57 +0200

    changed package name

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PaymentSDK",
+            name: "PayTabsSDK",
             targets: ["PaymentSDK"]),
     ],
     dependencies: [
